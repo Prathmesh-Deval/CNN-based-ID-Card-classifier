13bec7af1a29505adf26b7f70c27764abf56aabf
Prathmesh <70071334+Py-Pikachu@users.noreply.github.com>
2022-11-21 13:02:50 +0530

    Update Data Augmentation.py

--- Data/Data Augmentation.py
+++ Data/Data Augmentation.py
@@ -4,14 +4,18 @@ from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import ImageDataGenerator
 import cv2
 
-i=2669
-path = r'Aadhar'
+i=0
+
+#Provide path in which you want to perform Data augmentation
+
+path = r'aadhar'
+#path = r'pan'
+
+#Traverse throught that directory
 img_names = os.listdir(path)
 print(img_names)
-p=0
+
 for img_name in img_names:
-    if p == 100:
-        break
     img_path = os.path.join(path, img_name)
     image= cv2.imread(img_path)
 
@@ -21,21 +25,25 @@ for img_name in img_names:
     print('okay')
     datagen = ImageDataGenerator(
         #brightness_range=[0.4, 1.5],
-        #horizontal_flip = True,
-        rescale=1. / 255
+        horizontal_flip = True,
+        #rescale=1. / 255
         # zoom_range = 0.2
         #shear_range=0.2
         #rotation_range=90
-    )
+    ) #uncomment and use functions as per needed
+    
+    #pass our data into data generator
 
-    it = datagen.flow(s, batch_size=1)
+    gen1= datagen.flow(s, batch_size=1)
 
-    for j in range(1):
-        batch =it.next()
+    for j in range(10):
+        batch = gen1.next()
         image=batch[0].astype('uint8')
+        
+        #save new generated images
         a ='aadhar'+str(i)+'.jpeg'
         i+=1
-        path = 'Aadhar'
+        path = 'aadhar'
+        path = 'pan'
         cv2.imwrite(os.path.join(path, a), image)
-        p+=1
 
